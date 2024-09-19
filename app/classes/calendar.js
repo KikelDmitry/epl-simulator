0f5981484a3000df5211945b3bce32eb038fe7b7
Kikel Dmitry <kikeldmitry@gmail.com>
2024-09-19 23:00:47 +0300

    calendar in progress

--- app/classes/calendar.js
+++ app/classes/calendar.js
@@ -3,31 +3,59 @@ import { randNum } from "../services/random-number.js";
 export class Calendar {
   constructor(teams) {
     this.teams = teams;
-	this.matchesAmount = 0;
+    this.season = new Array(2); // 2 rounds
+    this.round = new Array(this.teams.length - 1); // 19 tours
+    this.tour = new Array(this.teams.length / 2); // 10 matches
+    this.match = new Array(2); // 2 teams
+    this.matchesAmount = 0;
   }
-  schedule() {
-    const teams = this.teams;
-    const schedule = [];
-    console.log(this.teams);
-    // creating rounds (2)
-    for (let r = 0; r < 2; r++) {
-      const round = [];
-      for (let i1 = 0; i1 < teams.length; i1++) {
-        for (let i2 = 0; i2 < teams.length; i2++) {
-          if (i1 !== i2) {
-			this.matchesAmount++;
-            const match = [teams[i1], teams[i2]];
-            round.push(match);
-          } else {
-            continue;
-          }
-        }
-      }
-      schedule.push(round);
+  createMatch() {
+    for (let i = 0; i < teams.length; i++) {
+      this.match.push();
     }
-    console.log(this.matchesAmount);
   }
+
+  createTour() {}
+  createRound() {}
+  createSchedule() {}
+
+  
+  schedule() {
+    this.season.push(this.round);
+    console.log(this.season);
+  }
+  // schedule() {
+  //   const teams = this.teams;
+  //   const season = [];
+  //   console.log(this.teams);
+  //   // creating rounds (2)
+  //   for (let r = 0; r < 2; r++) {
+  //     const round = [];
+  //     for (let t = 0; t < teams.length - 1; t++) {
+  //       const tour = [];
+  //       for (let i = 0; i < teams.length / 2; i++) {
+  //         const match = [teams[i], teams[i + 1]];
+  //         tour.push(match);
+  //       }
+  //       round.push(tour);
+  //     }
+  //     season.push(round);
+  //   }
+  //   console.log(season);
+  //   console.log("matches: ", this.matchesAmount);
+  // }
 }
+// for (let t1 = 0; t1 < teams.length; t1++) {
+//   for (let t2 = 0; t2 < teams.length; t2++) {
+//     if (t1 !== t2) {
+//       this.matchesAmount++;
+//       const match = [teams[t1], teams[t2]];
+//       tour.push(match);
+//     } else {
+//       continue;
+//     }
+//   }
+// }
 
 // const teams = [
 // 	{
@@ -39,8 +67,16 @@ export class Calendar {
 // const schedule = [
 //   // first round
 //   [
-//     [team1, team2],
-//     [team2, team3],
+//     //first tour
+//     [
+//       [team1, team2],
+//       [team3, team4],
+//     ],
+//     //second tour
+//     [
+//       [team1, team2],
+//       [team3, team4],
+//     ],
 //   ],
 //   // second round
 //   [
--- app/index.js
+++ app/index.js
@@ -40,12 +40,15 @@ import { $CONFIG } from "./config.js";
 
 const startSeason = async () => {
   // get teams
-  const teams = await fetch($CONFIG.teamsUrl)
-    .then((res) => res.json())
+  const teams = await fetch($CONFIG.teamsUrl).then((res) => res.json());
+
   // set schedule
   const schedule = new Calendar(teams).schedule();
   // play matches
   // show results
+
+  console.log(schedule);
+  
 };
 
 startSeason();
