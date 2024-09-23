ea7681d690ea1a161f98f55601cea5321ccacfce
Kikel Dmitry <kikeldmitry@gmail.com>
2024-09-23 21:36:53 +0300

    approach changed

--- app/classes/calendar.js
+++ app/classes/calendar.js
@@ -3,39 +3,27 @@ import { randNum } from "../services/random-number.js";
 export class Calendar {
   constructor(teams) {
     this.teams = teams;
-    this.season = new Array(2); // 2 rounds
-    this.round = new Array(this.teams.length - 1); // 19 tours
-    this.tour = []; // 10 matches
-    this.match = []; // 2 teams
+    this.matches = [];
   }
-  createMatch() {
-    const pair = [];
-
-    for (let i = 0; i < 2; i++) {
-      const randomTeam = randNum(0, this.teams.length - 1);
-      pair.push(this.teams[randomTeam]);
-      this.teams.splice(randomTeam, 1);
-    }
-
-    this.match = pair;
-  }
-
-  createTour() {
-    for (let i = 0; i < 10; i++) {
-      this.createMatch();
-      this.tour.push(this.match);
+  createAllMatches() {
+    for (let i = 0; i < this.teams.length; i++) {
+      for (let j = 0; j < this.teams.length; j++) {
+        if (this.teams[i] === this.teams[j]) continue;
+        this.matches.push([this.teams[i], this.teams[j]]);
+      }
     }
   }
-  createRound() {}
-  createSchedule() {}
-
-  comparePair(pair) {
-    if (!Array.isArray(pair)) throw new Error("pair should be an Array");
-    pair[0].shortName === pair[1].shortName ? true : false;
+  createTable() {
+    for (let i = 0; i < this.teams.length - 1; i++) {
+      for (let j = 0; j < this.season.length; j++) {
+                
+      }      
+    }
   }
+
   schedule() {
-    this.createTour();
-    console.log(this.tour);
+    this.createAllMatches();
+    console.log(this.matches);
   }
 }
 
