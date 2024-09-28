e729ea53620cd68256e8c8fe7d0da06a6640b3ca
Kikel Dmitry <kikeldmitry@gmail.com>
2024-09-28 13:50:14 +0300

    logos added

--- app/classes/calendar.js
+++ app/classes/calendar.js
@@ -1,27 +1,33 @@
 import { randNum } from "../services/random-number.js";
+import { sortByField } from "../services/sorting.js";
 
 export class Calendar {
   constructor(teams) {
     this.teams = teams;
     this.matches = [];
+    this.tour = [];
   }
-  createAllMatches() {
-    for (let i = 0; i < this.teams.length; i++) {
-      for (let j = 0; j < this.teams.length; j++) {
-        if (this.teams[i] === this.teams[j]) continue;
-        this.matches.push([this.teams[i], this.teams[j]]);
-      }
+  get teamsList() {
+    return this.teams.map((team) => team.alias);
+  }
+  // createAllMatches() {
+  //   for (let i = 0; i < this.teams.length; i++) {
+  //     for (let j = 0; j < this.teams.length; j++) {
+  //       if (this.teams[i] === this.teams[j]) continue;
+  //       this.matches.push([this.teams[i], this.teams[j]]);
+  //     }
+  //   }
+  // }
+
+  createTour() {
+    for (let i = 0; i < this.teamsList.length / 2; i++) {
+      
     }
   }
 
-  createTour() {
-    
-  }
-
   schedule() {
-    this.createAllMatches();
-    this.createTour();
-    console.log(this.matches);
+    console.log(sortByField(this.teams, 'name'));
+    console.log(this.tour);
   }
 }
 
--- app/services/sorting.js
+++ app/services/sorting.js
@@ -6,3 +6,7 @@ export const sorting = (teams) => {
     a.points > b.points ? 1 : b.points > a.points ? -1 : 0
   );
 };
+
+export const sortByField = (arr, field) => {
+  return arr.sort((a, b) => (a[field] > b[field] ? 1 : -1));
+};
